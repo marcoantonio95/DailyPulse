cc0a7d843a9b8059a8d9461fa459b484c83f2960
Marco Antonio <Marco.antonio9858@gmail.com>
2024-10-18 14:08:23 -0300

    remove invalid API key

--- shared/src/commonMain/kotlin/com/marcoantonio95/dailypulse/articles/data/ArticlesService.kt
+++ shared/src/commonMain/kotlin/com/marcoantonio95/dailypulse/articles/data/ArticlesService.kt
@@ -8,7 +8,7 @@ class ArticlesService(private val httpClient: HttpClient) {
 
     private val country = "us"
     private val category = "business"
-    private val apiKey = "25bd67501f3a40628f2a1d05fe8d8f86"
+    private val apiKey = ""
 
     suspend fun fetchArticles(): List<ArticleRaw> {
         val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey").body()
